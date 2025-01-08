fd774820a51845baa25a50d0a2ce11373c9188df
Cezaniltom <cezaniltom@outlook.com>
2025-01-08 20:34:46 -0300

    adicionando o regex validador de numeros

--- script.js
+++ script.js
@@ -2,11 +2,18 @@ const valorTotal = document.querySelector('#total-value')
 const addValor = document.querySelector('#initial-value')
 const recebeValor = document.querySelector('#valor')
 
+
+// Evanto que captura o valor inicial e mostra na tela
 addValor.addEventListener('click', () => {
 
-    const valorAtual = valorTotal.value;
+    const valorAtual = parseFloat(valorTotal.value.replace(/[^\d.-]/g, ''));
 
-    recebeValor.textContent = `R$ ${valorAtual}`;
-
+    if (isNaN(valorAtual)) {
+        alert('Por favor, adicione um valor válido.');        
+    } else {
+        recebeValor.textContent = `R$ ${valorAtual}`;
+    }
     valorTotal.value = '';
 })
+
+
