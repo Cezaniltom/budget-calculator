f0e52cd7db1ddd2d2c0bb67cb07ed8001b8b3641
Cezaniltom <cezaniltom@outlook.com>
2025-01-11 23:11:44 -0300

    corrigindo bug do localstorage

--- script.js
+++ script.js
@@ -3,36 +3,31 @@ const addValor = document.querySelector('#initial-value')
 const recebeValor = document.querySelector('#valor')
 
 
+// Evento que captura o valor inicial e mostra na tela
+addValor.addEventListener('click', () => {
+    const valorAtual = parseFloat(valorTotal.value.replace(/[^\d.-]/g, ''));
+    if (isNaN(valorAtual)) {
+        alert('Por favor, adicione um valor válido.');
+    } else {
+        localStorage.setItem('ValorInicial', valorAtual);
+        recebeValor.textContent = valorAtual.toLocaleString('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+        });
+    }    
+
+    valorTotal.value = '';
+})
+
+
+// Persistindo os dados no localStorage
 const valueStorage = localStorage.getItem('ValorInicial');
 if(valueStorage) {
     recebeValor.textContent = parseFloat(valueStorage).toLocaleString('pt-BR', {
         style: 'currency',
         currency: 'BRL',
     });
-} else {
-    recebeValor = "Nenhum valor adicionado"
 }
-
-
-// Evento que captura o valor inicial e mostra na tela
-addValor.addEventListener('click', () => {
-    const valorAtual = parseFloat(valorTotal.value.replace(/[^\d.-]/g, ''));
-
-    if (isNaN(valorAtual)) {
-        alert('Por favor, adicione um valor válido.');        
-    } else {
-        localStorage.setItem('ValorInicial', valorAtual);
-
-        recebeValor.textContent = valorAtual.toLocaleString('pt-BR', {
-            style: 'currency',
-            currency: 'BRL',
-        });
-    }
-
-    
-
-    valorTotal.value = '';
-})
 
 
 // recebe o valor da despesa
