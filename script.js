bbf9dac1979b2dbdc3905fe38f810d3a7a369001
Cezaniltom <cezaniltom@outlook.com>
2025-01-11 23:04:00 -0300

    Persistindo os dados no localstorage

--- script.js
+++ script.js
@@ -3,18 +3,30 @@ const addValor = document.querySelector('#initial-value')
 const recebeValor = document.querySelector('#valor')
 
 
+const valueStorage = localStorage.getItem('ValorInicial');
+if(valueStorage) {
+    recebeValor.textContent = parseFloat(valueStorage).toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+    });
+} else {
+    recebeValor = "Nenhum valor adicionado"
+}
+
+
 // Evento que captura o valor inicial e mostra na tela
 addValor.addEventListener('click', () => {
-
     const valorAtual = parseFloat(valorTotal.value.replace(/[^\d.-]/g, ''));
 
     if (isNaN(valorAtual)) {
         alert('Por favor, adicione um valor válido.');        
     } else {
-        recebeValor.textContent = `${valorAtual.toLocaleString('pt-BR', {
+        localStorage.setItem('ValorInicial', valorAtual);
+
+        recebeValor.textContent = valorAtual.toLocaleString('pt-BR', {
             style: 'currency',
             currency: 'BRL',
-        })}`;
+        });
     }
 
     
